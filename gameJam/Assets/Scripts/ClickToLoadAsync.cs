894a52fb4af31dae2d2c62313c382f79eb9e00f7
Gabriel Doyon <gabriel.olif@gmail.com>
2017-09-23 14:39:50 -0400

    add menu mookup

---
+++ gameJam/Assets/Scripts/ClickToLoadAsync.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ClickToLoadAsync : MonoBehaviour {
+
+    public Slider loadingBar;
+    public GameObject loadingImage;
+
+    private AsyncOperation async;
+
+    public void ClickAsync(int level)
+            {
+        loadingImage.SetActive(true);
+        StartCoroutine(LoadLevelWithBar(level));
+    }
+
+    IEnumerator LoadLevelWithBar (int level)
+    {
+        async = Application.LoadLevelAsync(level);
+        while(!async.isDone)
+        {
+            loadingBar.value = async.progress;
+            yield return null;
+        }
+    }
+}
---
+++ gameJam/Assets/Scripts/Option_Menu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Option_Menu : MonoBehaviour
+{
+    public GameObject optionVisibility;
+
+    public void ToggleOption()
+    {
+        optionVisibility.SetActive(false);
+    }
+}
