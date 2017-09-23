62a74d69d2b679497546cded02bb93b0219534b7
geekymoose <constantin.masson@umontreal.ca>
2017-09-23 12:25:57 -0400

    Add hammer attack for player

---
+++ gameJam/Assets/Scripts/HammerAttack.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerAttack : MonoBehaviour {
+    // ------------------------------------------------------------------------
+    // Variables
+    // ------------------------------------------------------------------------
+    public SphereCollider pokeHitPoint;
+    public SphereCollider hammerHitPoint;
+    public SphereCollider hammerCrushPoint;
+
+    // Pole attack
+    public float pokeColdown            = 1f; // Reload time in seconds
+    private float pokeCurrentTimer      = 0f;
+    private bool pokeIsReady            = true;
+    public float pokeForce              = 100f;
+
+    
+    // Hammer attack
+    public float hammerColdown          = 4f; // Reload time in seconds
+    private float hammerCurrentTimer    = 0f;
+    private bool hammerIsReady          = true;
+    public float hammerForceVertical    = 100f;
+    public float hammerForceHorizontal  = 100f;
+
+
+    // ------------------------------------------------------------------------
+    // Functions
+    // ------------------------------------------------------------------------
+
+	// Update is called once per frame
+	void Update () {
+        this.updateAllColdowns();
+
+        // Handle inputs
+        if(Input.GetKeyDown(KeyCode.E)) {
+            this.pokeAttack();
+        }
+        else if(Input.GetKeyDown(KeyCode.T)) {
+            this.bigHammerAttack();
+        }
+    }
+
+    /** Process a simple poke attack */
+    private void pokeAttack() {
+        if(!this.pokeIsReady) {
+            // Not available yet
+            // TODO Play a sound or something (UI warning..)?
+            return;
+        }
+        this.pokeCurrentTimer = 0;
+        this.pokeIsReady = false;
+        //Debug.Log(overlap.center);
+        Collider[] hitColliders = Physics.OverlapSphere(pokeHitPoint.gameObject.transform.position, pokeHitPoint.radius);
+        for(int i = 0; i < hitColliders.Length; i++) {
+            if(hitColliders[i].gameObject.CompareTag("FatAnimal")) {
+                Vector3 dir = hitColliders[i].transform.position - transform.position;
+                dir = Vector3.Normalize(dir);
+                hitColliders[i].gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+                hitColliders[i].gameObject.GetComponent<Rigidbody>().AddForce(dir * pokeForce);
+            }
+        }
+    }
+
+    /** Process a big hammer attack */
+    private void bigHammerAttack() {
+        if(!this.hammerIsReady) {
+            // Not available yet
+            // TODO Play a sound or something (UI warning..)?
+            return;
+        }
+        this.hammerCurrentTimer = 0;
+        this.hammerIsReady = false;
+        Collider[] hitColliders = Physics.OverlapSphere(hammerHitPoint.gameObject.transform.position, hammerHitPoint.radius);
+        for(int i = 0;i < hitColliders.Length;i++) {
+            if(hitColliders[i].gameObject.CompareTag("FatAnimal")) {
+                Rigidbody rb = hitColliders[i].gameObject.GetComponent<Rigidbody>();
+                Vector3 dir = hitColliders[i].transform.position - hammerHitPoint.gameObject.transform.position;
+                dir = Vector3.Normalize(dir);
+                rb.velocity = new Vector3(0, 0, 0);
+                rb.AddForce((Vector3.up * hammerForceVertical));
+                rb.AddForce((dir * hammerForceHorizontal));
+            }
+        }
+    }
+
+    private void updateAllColdowns() {
+        // Poke coldown
+        this.pokeCurrentTimer += Time.deltaTime;
+        if(this.pokeCurrentTimer >= this.pokeColdown) {
+            this.pokeIsReady = true;
+        }
+
+        // Hammer coldown
+        this.hammerCurrentTimer += Time.deltaTime;
+        if(this.hammerCurrentTimer >= this.hammerColdown) {
+            this.hammerIsReady = true;
+        }
+    }
+}
---
+++ gameJam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    // ------------------------------------------------------------------------
+    // Variables
+    // ------------------------------------------------------------------------
+    public float walkingSpeed = 5f;
+    private bool isWalking;
+
+
+    // ------------------------------------------------------------------------
+    // Functions
+    // ------------------------------------------------------------------------
+
+    // Update is called once per frame
+    void Update() {
+        float horizontal    = Input.GetAxisRaw("Horizontal");
+        float verical       = Input.GetAxisRaw("Vertical");
+        this.handleMovement(horizontal, verical);
+    }
+
+    private void handleMovement(float horizontal, float vertical) {
+        this.isWalking = false;
+        if(horizontal != 0.0f || vertical != 0.0f) {
+            this.isWalking = true;
+            float angle = Mathf.Atan2(horizontal, vertical) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(new Vector3(0f, angle, 0f));
+            transform.position += transform.forward * Time.deltaTime * walkingSpeed;
+        }
+    }
+}
