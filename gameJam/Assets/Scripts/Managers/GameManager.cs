a8dff7ae7edb758c2320d978372dfbee1f955bfd
geekymoose <constantin.masson@umontreal.ca>
2017-09-23 20:17:33 -0400

    Update the balance calculation

--- gameJam/Assets/Scripts/Managers/GameManager.cs
+++ gameJam/Assets/Scripts/Managers/GameManager.cs
@@ -10,6 +10,8 @@ public class GameManager : MonoBehaviour {
      * Balance system: il full dogs, balance is -100
      * If full cats, balance is 100
      * If perfect balance, value is 0
+     * 
+     * Warning: In UI, dogs are at the right since netative rotation is clockwise in our case 
      */
 
     public GameObject ui_balancePivot;
@@ -19,12 +21,12 @@ public class GameManager : MonoBehaviour {
     public float spawnSpeedMaxInSecond = 0.1f;
     public float spawnSpeedCurrentInSecond;
 
-    public int nbAnimals = 0;
-    public int nbCats = 0;
-    public int nbDogs = 0;
+    private int nbAnimals = 0;
+    private int nbCats = 0;
+    private int nbDogs = 0;
 
-    public float percentNbCats = 0;
-    public float percentNbDogs = 0;
+    private float percentNbCats = 0;
+    private float percentNbDogs = 0;
     
     public float thresholdSuccess = 20; // % away from total ballance that is still ok
     public float thresholdWarning = 40;
@@ -32,10 +34,9 @@ public class GameManager : MonoBehaviour {
     public float thresholdLoose = 95;
 
     public float balanceSensibility = 1;
-    public float balanceGapFactor = 1;
-    public float currentBallanceLevel = 0;
+    private float currentBallanceLevel = 0;
 
-    public int currentAnimalTrence = 0; // -1 if dogs, 1 if cats, 0 if perfect balance
+    private int currentAnimalTrence = 0; // -1 if dogs, 1 if cats, 0 if perfect balance
 
 
     // ------------------------------------------------------------------------
@@ -63,8 +64,8 @@ public class GameManager : MonoBehaviour {
         }
         
         this.updateBalance();
+        this.calculateNewBalance();
         this.processBalanceActions();
-        Debug.Log(Mathf.Log10(nbAnimals));
     }
 
     /**
@@ -79,45 +80,46 @@ public class GameManager : MonoBehaviour {
             this.percentNbDogs = 0;
             this.currentAnimalTrence = 0;
             if(old != currentAnimalTrence) {
-                //TODO Play event
+                //TODO Play event (He just reach this beautiful balance)
             }
             float expectedNewBalance = 0;
-            Debug.Log(balanceSensibility * Time.deltaTime);
             this.currentBallanceLevel = Mathf.Lerp(currentBallanceLevel, expectedNewBalance, balanceSensibility * Time.deltaTime);
             return;
         }
+
+        // Update values
         this.percentNbCats = ((float)nbCats / (float)nbAnimals) * 100;
         this.percentNbDogs = ((float)nbDogs / (float)nbAnimals) * 100;
-
-        // Update balance trence
         int oldTrence = currentAnimalTrence;
         currentAnimalTrence = percentNbCats > percentNbDogs ? 1 : -1;
+        
+        // Special case of switching balance site (cat / dog or total balance)
+        if(oldTrence != currentAnimalTrence) {
+            if(currentAnimalTrence == 0) {
+                // Perfect balance
+                // TODO
+            }
+            else if (currentAnimalTrence == -1) {
+                // Dog side
+                // TODO
+            }
+            else {
+                // Cat side
+                // TODO
+            }
+        }
+    }
+    
+    /**
+     * Calculate the new Balance value.
+     * Must be called after all animal data are up to date.
+     */
+    private void calculateNewBalance() {
+        float balanceGap = nbCats - nbDogs;
 
-        // Update current balance value
-        if(percentNbCats == percentNbDogs) {
-            currentAnimalTrence = 0; // Special case (The perfect ballance, but really rare)
-            if(oldTrence != currentAnimalTrence) {
-                //TODO Play special event like sound or something (To emphazise the change)
-            }
-            float expectedNewBalance = 0;
-            this.currentBallanceLevel = Mathf.Lerp(currentBallanceLevel, expectedNewBalance, balanceSensibility * Time.deltaTime);
-        }
-        else if(percentNbDogs > percentNbCats) {
-            // You are a dog lover, but the ballance may be broken
-            if(oldTrence != currentAnimalTrence) {
-                //TODO
-            }
-            float expectedNewBalance = -percentNbDogs;
-            this.currentBallanceLevel = Mathf.Lerp(currentBallanceLevel, expectedNewBalance, balanceSensibility * Time.deltaTime);
-        }
-        else {
-            // You are a cat lover, but the ballance may be broken
-            if(oldTrence != currentAnimalTrence) {
-                //TODO
-            }
-            float expectedNewBalance = percentNbCats;
-            this.currentBallanceLevel = Mathf.Lerp(currentBallanceLevel, expectedNewBalance, balanceSensibility * Time.deltaTime);
-        }
+        float expectedNewBalance = balanceGap * 10;
+        expectedNewBalance = Mathf.Clamp(expectedNewBalance, -90, 90);
+        this.currentBallanceLevel = Mathf.Lerp(currentBallanceLevel, expectedNewBalance, balanceSensibility * Time.deltaTime);
     }
 
     private void processBalanceActions() {
